d9fd463c95341dace8639fb0e31332e6077b4fb5
jennifer <jshiyuezhen@gmail.com>
2015-05-21 21:55:40 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import time
 
-ISOTIMEFORMAT=’%Y-%m-%d %X’
+ISOTIMEFORMAT='%Y-%m-%d %X'
 print time.time()
 print time.localtime()
 print time.strftime( ISOTIMEFORMAT, time.localtime( time.time() ) )
